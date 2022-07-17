decf63aa0c17b47658594dd1a7ae8d0de4650ebd
Nikita Gugel <googelfist@gmail.com>
2022-07-17 15:29:11 +0500

    add okhttp login interceptor

--- app/src/main/java/com/example/sass/di/RetrofitModule.kt
+++ app/src/main/java/com/example/sass/di/RetrofitModule.kt
@@ -4,6 +4,8 @@ import com.example.sass.data.datasource.remote.picture.PicturesRetrofitService
 import com.example.sass.data.datasource.remote.user.UserRetrofitService
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -13,10 +15,27 @@ class RetrofitModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return loggingInterceptor
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
     }
 
